769fa0542eaf118b0b88c0b7492df2d8592aa164
Jackson Levine <jack@netbug.us>
2025-01-28 15:56:15 -0800

    crank up render distance now!

--- src/world/WorldRenderer.h
+++ src/world/WorldRenderer.h
@@ -100,7 +100,7 @@ struct UsedChunkInfo
 class WorldRenderer {
 public:
     static constexpr int chunkSize = 16;
-    static constexpr int renderDistance = 5;
+    static constexpr int renderDistance = 10;
     static constexpr int maxChunks = ((renderDistance*2) * (renderDistance*2)) + renderDistance*10;
 
     std::vector<ChunkGLInfo> chunkPool;
