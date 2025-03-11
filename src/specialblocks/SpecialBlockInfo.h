b09586afb488e0a96e4d5b160f570e6cb8770074
Jackson Levine <jack@netbug.us>
2025-03-11 00:34:38 -0700

    start specialblocks

--- src/specialblocks/SpecialBlockInfo.h
+++ src/specialblocks/SpecialBlockInfo.h
@@ -4,12 +4,13 @@
 
 #ifndef SPECIALBLOCKINFO_H
 #define SPECIALBLOCKINFO_H
+#include "../world/WorldRenderer.h"
 
 
-// class SpecialBlockInfo
-// {
-// public:
-//     virtual void get
-// };
+class SpecialBlockInfo
+{
+public:
+    virtual void meshMeDaddy(UsableMesh &mesh, uint32_t block, IntTup position, PxU32 &index, PxU32 &tindex) = 0;
+};
 
 #endif //SPECIALBLOCKINFO_H
