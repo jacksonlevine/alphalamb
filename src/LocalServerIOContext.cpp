bbaec597a93cfa1564a4359a9efcf38e440dc9ea
Jackson Levine <jack@netbug.us>
2025-05-20 11:29:56 -0700

    set threadpool size

--- src/LocalServerIOContext.cpp
+++ src/LocalServerIOContext.cpp
@@ -4,7 +4,7 @@
 #include "LocalServerIOContext.h"
 std::unique_ptr<boost::asio::io_context> localserver_io_context = nullptr;
 
-boost::asio::thread_pool localserver_threadpool;
+boost::asio::thread_pool localserver_threadpool(4);
 
 std::atomic<bool> localserver_running = {false};
 std::thread localserver_thread;
